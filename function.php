af1359982b7acaa91b0bf4490d900305eaafe78b
LycanCoder <email.yong@qq.com>
2017-09-01 10:13:27 +0800

    Update function.php
    Update check_idcard function

--- function.php
+++ function.php
@@ -8,7 +8,7 @@
  * @check_chinese_character()	是否含有汉字
  * @check_invalide_str()	是否含有非法字符
  * @check_post_num()		邮证编码
- * @check_personal_card()	身份证号码
+ * @check_idcard()		身份证号码
  * @check_ip()			验证IP地址
  * @check_book_isbn()		出版物的ISBN号
  * @check_mobile()		手机号码(中国大陆区)
@@ -93,15 +93,61 @@ function check_post_num($num) {
 /*
  * 正则表达式验证身份证号码
  * @author Lycan
- * @time 2017/07/07
- * @param int $num 所要验证的身份证号码
+ * @time 2017/09/01
+ * @param string $id 身份证号码
  * @return boolean
  */
-function check_personal_card($num) {
-	if (!$num) {
+function check_idcard($id) {
+	$id = strtoupper($id); // 转换成大写
+	$regx = "/(^\d{15}$)|(^\d{17}([0-9]|X)$)/"; // 正则
+	$arr_split = array();
+	
+	// 身份证号码位数不正确
+	if(!preg_match($regx, $id)) { 
 		return false;
 	}
-	return preg_match('/^[\d]{15}$|^[\d]{18}$/', $num) ? true : false;
+	
+	if(strlen($id) == 15) { // 早先的15位的身份证号
+		$regx = "/^(\d{6})+(\d{2})+(\d{2})+(\d{2})+(\d{3})$/";
+		@preg_match($regx, $id, $arr_split);
+		$dtm_birth = "19" . $arr_split[2] . "/" . $arr_split[3] . "/" . $arr_split[4];
+		if(!strtotime($dtm_birth)) { // 检查生日日期是否正确
+			return false;
+		} else {
+			return true;
+		}
+	} else { // 18位身份证号
+		$regx = "/^(\d{6})+(\d{4})+(\d{2})+(\d{2})+(\d{3})([0-9]X)$/";
+		@preg_match($regx, $id, $arr_split);
+		$dtm_birth = $arr_split[2] . "/" . $arr_split[3] . "/" . $arr_split[4];
+		if(!strtotime($dtm_birth)) { // 检查生日日期是否正确
+			return false;
+		} else {
+			// 检查18位身份证的校验码是否正确
+			// 校验码按照ISO 7064:MOD 11-2的规定生成，X可以认为是数字10
+			
+			// 加权因子
+			$factor = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+			
+			// 校验码对应值
+			$verify_num_list = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+			
+			$checksum = 0;
+			
+			for($i = 0; $i < 17; $i++) {
+				$checksum += (int)$id{$i} * $factor[$i];
+			}
+			
+			$mod = $checksum % 11;
+			$val_num = $verify_num_list[$mod];
+			
+			if($val_num != substr($id, 17, 1)) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
 }
 
 /*
